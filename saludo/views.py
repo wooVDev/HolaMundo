118fa0faf862051ce0238ce3c7c453f31fa6f5e8
wooVDev <luisthielen245@gmail.com>
2025-05-23 20:50:23 -0400

    Hola mundo

--- saludo/views.py
+++ saludo/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hola_mundo(request):
-    return HttpResponse("Hola a toditos los del grupo de sistema pa y a todo aquel que clone esto...")
+    return HttpResponse("Hola a toditos los del grupo de sistema pa y a todo aquel que clone esto :D...")
